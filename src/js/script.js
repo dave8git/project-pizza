6e02f80e205ffd203a86c6f36b270327037a98f5
dave8git@gmail.com <dave8git@gmail.com>
2020-07-15 19:46:01 +0200

    fix eslint errors

--- src/js/script.js
+++ src/js/script.js
@@ -147,12 +147,13 @@
         event.preventDefault();
 
         thisProduct.processOrder();
-        thisProduct.addToCart(); 
+        thisProduct.addToCart();
       });
       // console.log('initOrderForm - thisProduct', thisProduct);
     }
     processOrder() {
       const thisProduct = this;
+      thisProduct.params = {};
       const formData = utils.serializeFormToObject(thisProduct.form);
       let price = thisProduct.data.price;
       for (let paramId in thisProduct.data.params) {
@@ -173,6 +174,13 @@
           const activeImages = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
           // console.log('activeImages', activeImages);
           if (optionSelected) {
+            if (!thisProduct.params[paramId]) {
+              thisProduct.params[paramId] = {
+                label: param.label,
+                options: {},
+              };
+            }
+            thisProduct.params[paramId].options[optionId] = option.label;
             for (let activeImage of activeImages) {
               activeImage.classList.add(classNames.menuProduct.imageVisible);
             }
@@ -183,8 +191,12 @@
           }
         }
       }
-      price *= thisProduct.amountWidget.value; /* multiply price by amount */
-      thisProduct.priceElem.innerHTML = price;
+      /* multiply price by amount */
+      thisProduct.priceSingle = price;
+      thisProduct.price = thisProduct.priceSingle * thisProduct.amountWidget.value;
+
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+      thisProduct.priceElem.innerHTML = thisProduct.price;
     }
     initAmountWidget() {
       const thisProduct = this;
@@ -194,7 +206,7 @@
       });
     }
     addToCart() {
-      const thisProduct = this; 
+      const thisProduct = this;
 
       app.cart.add(thisProduct);
     }
@@ -221,13 +233,13 @@
     setValue(value) {
       const thisWidget = this;
       const newValue = parseInt(value);
-    
+
       /* TODO: Add validation */
-      if((thisWidget.value != value) && (newValue >= settings.amountWidget.defaultMin) && (newValue <= settings.amountWidget.defaultMax)) {
+      if ((thisWidget.value != value) && (newValue >= settings.amountWidget.defaultMin) && (newValue <= settings.amountWidget.defaultMax)) {
         thisWidget.value = newValue;
         thisWidget.announce();
       }
-     
+
       thisWidget.input.value = thisWidget.value;
 
     }
@@ -251,23 +263,23 @@
       thisWidget.element.dispatchEvent(event);
     }
   }
-  class Cart{
-    constructor(element){
+  class Cart {
+    constructor(element) {
       const thisCart = this;
 
       thisCart.products = [];
-      thisCart.getElements(element); 
-      thisCart.initActions(); 
+      thisCart.getElements(element);
+      thisCart.initActions();
 
       console.log('new Cart', thisCart);
     }
 
-    getElements(element){
+    getElements(element) {
       const thisCart = this;
 
-      thisCart.dom = {}; 
-      thisCart.dom.wrapper = element; 
-      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger); 
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
       console.log('thisCart.dom.toggleTrigger', thisCart.dom.toggleTrigger);
     }
     initActions() {
@@ -275,9 +287,9 @@
       thisCart.dom.toggleTrigger.addEventListener('click', function (event) {
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
-      }); 
+      });
     }
-    add(menuProduct){
+    add(menuProduct) {
       //const thisCart = this;
 
       console.log('adding product', menuProduct);
@@ -298,11 +310,12 @@
 
       thisApp.data = dataSource;
     },
-    initCart: function(){
+    initCart: function () {
       const thisApp = this;
 
-      const cartElem = document.querySelector(select.containerOf.cart); 
-      thisApp.cart = new Cart(cartElem); 
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+      console.log(thisApp);
     },
     init: function () {
       const thisApp = this;
